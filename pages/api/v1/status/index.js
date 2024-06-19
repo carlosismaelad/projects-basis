9eb93fd24e288832f988c2f8fb831533861a8911
Carlos Dourado <110005935+carlosismaelad@users.noreply.github.com>
2024-06-19 11:57:33 +0000

    Add o arquivo `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são acima da média!" });
+  response.status(200).json({ chave: "Endpoint respondendo com sucesso!" });
 }
 
 export default status;
